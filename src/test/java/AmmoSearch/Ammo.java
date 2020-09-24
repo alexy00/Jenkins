0b7a238d5232dd88bd407c3870af4c43bfc3abc8
Alex <alabandy@gmail.com>
2020-09-24 19:11:32 -0400

    initial commit

---
+++ src/test/java/AmmoSearch/Ammo.java
@@ -0,0 +1,41 @@
+package AmmoSearch;
+
+import Properties.Logins;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+import java.io.IOException;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
+
+public class Ammo {
+
+    @BeforeMethod
+    void login()throws IOException{
+        String baseUrl = Logins.Url();
+        open(baseUrl);
+    }
+    @Test
+    void CaliberSelection45ACP() {
+        SelenideElement ammo = $(By.xpath("//button[contains(text(),'Ammo')]")).waitUntil(Condition.visible, 1000);
+        ammo.hover();
+        SelenideElement acp = $(By.linkText("45 ACP")).waitUntil(Condition.visible, 2000);
+        acp.click();
+        String currentURL = url();
+        System.out.println(currentURL);
+        assert currentURL.contains("ACP");
+        screenshot("45 ACP");
+    }
+        @Test
+        void CaliberSelection12Gage(){
+            SelenideElement ammo = $(By.xpath("//button[contains(text(),'Ammo')]")).waitUntil(Condition.visible,1000);
+            ammo.hover();
+            SelenideElement twelve = $(By.linkText("12 Gauge")).waitUntil(Condition.visible,2000);
+            twelve.click();
+            String currentURL = url();
+            System.out.println(currentURL);
+            assert currentURL.contains("12+Gauge");
+            screenshot("12 Gauge");
+    }
+}
---
+++ src/test/java/FirearmsSearch/Firearms.java
@@ -0,0 +1,30 @@
+package FirearmsSearch;
+
+import Properties.Logins;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
+
+public class Firearms {
+    @BeforeMethod
+    void login()throws IOException {
+        String baseUrl = Logins.Url();
+        open(baseUrl);
+    }
+    @Test
+    void CaliberSelection45ACP() {
+        SelenideElement firearms = $(By.xpath("//button[contains(text(),'Firearms')]")).waitUntil(Condition.visible, 1000);
+        firearms.hover();
+        SelenideElement nine = $(By.linkText("9MM")).waitUntil(Condition.visible, 2000);
+        nine.click();
+        String currentURL = url();
+        System.out.println(currentURL);
+        assert currentURL.contains("9mm");
+        screenshot("9MM");
+    }
+}
