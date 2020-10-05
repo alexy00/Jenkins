15b5b0483aad2a79cd423d9e4e8fc097e2d6cd0a
Alex <alabandy@gmail.com>
2020-10-05 16:01:37 -0400

    initial commit

--- src/test/java/Properties/FotoPath.java
+++ src/test/java/Properties/FotoPath.java
@@ -3,7 +3,7 @@ package Properties;
 import java.io.IOException;
 
 public class FotoPath {
-    public static String Path()throws IOException {
+    public static String Path(){
         String fotoPath = "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Ammo/build/reports/tests/Fotos";
         return fotoPath;
     }
