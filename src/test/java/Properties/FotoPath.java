091f1ef02e122cf75ea27ec683ac71b6654e478c
Alex <alabandy@gmail.com>
2020-10-05 16:11:33 -0400

    initial commit

--- src/test/java/Properties/FotoPath.java
+++ src/test/java/Properties/FotoPath.java
@@ -2,8 +2,7 @@ package Properties;
 
 public class FotoPath {
     public static String Path(){
-        String fotoPath = "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Ammo/build/reports/tests/Fotos";
-        return fotoPath;
+        return "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Ammo/build/reports/tests/Fotos";
     }
 
 }
